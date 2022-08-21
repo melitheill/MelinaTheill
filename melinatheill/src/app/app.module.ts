514ea09f193f10a4e7a989c0e768859b31b266c6
melina <meli.theill@gmail.com>
2022-08-21 18:53:29 -0300

    push
    push

---
+++ melinatheill/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { LogoComponent } from './componentes/logo/logo.component';
+import { SobremiComponent } from './componentes/sobremi/sobremi.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { SoftskillsComponent } from './componentes/softskills/softskills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent,
+    BannerComponent,
+    LogoComponent,
+    SobremiComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SoftskillsComponent,
+    ProyectosComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
